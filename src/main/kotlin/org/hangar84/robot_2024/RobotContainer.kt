b453735441cd5ec24d743627c7efce016631a58e
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2024-03-18 23:27:35 -0600

    feat: add `PathPlanner`-based auto

--- src/main/kotlin/org/hangar84/robot_2024/RobotContainer.kt
+++ src/main/kotlin/org/hangar84/robot_2024/RobotContainer.kt
@@ -5,10 +5,17 @@
 
 package org.hangar84.robot_2024
 
+import com.pathplanner.lib.auto.AutoBuilder
+import com.pathplanner.lib.util.ReplanningConfig
 import edu.wpi.first.cameraserver.CameraServer
+import edu.wpi.first.math.geometry.Pose2d
+import edu.wpi.first.math.kinematics.ChassisSpeeds
+import edu.wpi.first.wpilibj.DriverStation
+import edu.wpi.first.wpilibj.smartdashboard.SendableChooser
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.Commands
-import org.hangar84.robot_2024.commands.DriveDistanceCommand
+import edu.wpi.first.wpilibj2.command.InstantCommand
 import org.hangar84.robot_2024.subsystems.DriveSubsystem
 import org.hangar84.robot_2024.subsystems.LauncherSubsystem
 import org.hangar84.robot_2024.wrappers.InvertableCommandXboxController
@@ -16,19 +23,33 @@ import org.hangar84.robot_2024.wrappers.InvertableCommandXboxController
 
 object RobotContainer {
     private val controller = InvertableCommandXboxController(0)
+    private var autoChooser: SendableChooser<Command>? = null
 
     val autonomousCommand: Command
         get() {
-            return DriveDistanceCommand(12.0 * 5.0)
+            return autoChooser?.selected ?: InstantCommand()
         }
 
 
     init {
-        // We disregard the returned UsbCamera instances as we don't need them
+        // We disregard the returned UsbCamera instances, as we don't need them
         CameraServer.startAutomaticCapture("Front Camera", 0)
         CameraServer.startAutomaticCapture("Rear Camera", 1)
 
         configureBindings()
+
+        AutoBuilder.configureRamsete(
+            { DriveSubsystem.pose },
+            { pose: Pose2d -> DriveSubsystem.resetPose(pose) },
+            { DriveSubsystem.chassisSpeeds },
+            { speeds: ChassisSpeeds -> DriveSubsystem.driveRelative(speeds) },
+            ReplanningConfig(),
+            { DriverStation.getAlliance()?.get() == DriverStation.Alliance.Red },
+            DriveSubsystem
+        )
+
+        autoChooser = AutoBuilder.buildAutoChooser()
+        SmartDashboard.putData("Autonomous Routine", autoChooser)
     }
 
     /**
