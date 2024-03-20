285f4b24df3785b77dc28803709f23065cf60b07
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2024-03-20 11:28:24 -0600

    feat: add `SimpleMotorFeedforward` and dashboard entries

--- src/main/kotlin/org/hangar84/robot_2024/subsystems/DriveSubsystem.kt
+++ src/main/kotlin/org/hangar84/robot_2024/subsystems/DriveSubsystem.kt
@@ -8,21 +8,47 @@ package org.hangar84.robot_2024.subsystems
 import com.ctre.phoenix.motorcontrol.can.WPI_TalonSRX
 import com.ctre.phoenix.motorcontrol.can.WPI_VictorSPX
 import edu.wpi.first.math.controller.PIDController
+import edu.wpi.first.math.controller.SimpleMotorFeedforward
 import edu.wpi.first.math.geometry.Pose2d
 import edu.wpi.first.math.geometry.Rotation2d
 import edu.wpi.first.math.kinematics.ChassisSpeeds
 import edu.wpi.first.math.kinematics.DifferentialDriveKinematics
 import edu.wpi.first.math.kinematics.DifferentialDriveOdometry
+import edu.wpi.first.networktables.NetworkTableInstance
 import edu.wpi.first.wpilibj.ADIS16470_IMU
 import edu.wpi.first.wpilibj.CounterBase
 import edu.wpi.first.wpilibj.Encoder
 import edu.wpi.first.wpilibj.drive.DifferentialDrive
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard
 import edu.wpi.first.wpilibj2.command.Subsystem
-import kotlin.math.abs
+
+data object DataTable {
+    private val table = NetworkTableInstance.getDefault().getTable("DriveData")
+
+    val leftVelocityEntry = table.getEntry("Left Velocity")
+    val rightVelocityEntry = table.getEntry("Right Velocity")
+
+    val leftVoltageEntry = table.getEntry("Left Voltage")
+    val rightVoltageEntry = table.getEntry("Right Voltage")
+
+    val staticGainEntry = table.getEntry("Static Gain")
+    val velocityGainEntry = table.getEntry("Velocity Gain")
+    val accelerationGainEntry = table.getEntry("Acceleration Gain")
+}
 
 object DriveSubsystem : Subsystem {
-    private val leftMotor = WPI_TalonSRX(0)
-    private val rightMotor = WPI_TalonSRX(1)
+    private const val METER_CONVERSION_FACTOR = 0.0254
+
+    private const val TRACK_WIDTH = 21.8 * METER_CONVERSION_FACTOR
+    private const val PULSES_PER_REVOLUTION = 8192.0
+    private const val WHEEL_DIAMETER = 6.0 * METER_CONVERSION_FACTOR
+    private const val WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI
+    private const val GEAR_RATIO = 1.0 / 1.0
+    private const val DISTANCE_PER_PULSE = WHEEL_CIRCUMFERENCE / (PULSES_PER_REVOLUTION * GEAR_RATIO)
+
+
+    internal val leftMotor = WPI_TalonSRX(0)
+    internal val rightMotor = WPI_TalonSRX(1)
 
     private val leftFollowerMotor = WPI_VictorSPX(0)
     private val rightFollowerMotor = WPI_VictorSPX(1)
@@ -36,25 +62,6 @@ object DriveSubsystem : Subsystem {
     internal val leftEncoder = Encoder(0, 1, true, CounterBase.EncodingType.k4X)
     internal val rightEncoder = Encoder(2, 3, false, CounterBase.EncodingType.k4X)
 
-    // TODO: Tune drive PID values
-    private const val DRIVE_P = 0.0
-    private const val DRIVE_I = 0.0
-    private const val DRIVE_D = 0.0
-
-    internal val leftPIDController = PIDController(DRIVE_P, DRIVE_I, DRIVE_D)
-    internal val rightPIDController = PIDController(DRIVE_P, DRIVE_I, DRIVE_D)
-
-    private const val PULSES_PER_REVOLUTION = 8192.0
-    private const val WHEEL_DIAMETER = 6.0
-    private const val WHEEL_CIRCUMFERENCE = WHEEL_DIAMETER * Math.PI
-    private const val GEAR_RATIO = 8.46 / 1.0
-    private const val DISTANCE_PER_PULSE = WHEEL_CIRCUMFERENCE / (PULSES_PER_REVOLUTION * GEAR_RATIO)
-    private const val METER_CONVERSION_FACTOR = 0.0254
-    private const val INCH_CONVERSION_FACTOR = 1.0 / METER_CONVERSION_FACTOR
-
-    // TODO: Tune drift correction PID values and/or deadband
-    private val driftCorrectionPID = PIDController(0.0, 0.0, 0.0)
-    private const val ROTATION_DEADBAND = 0.075
 
     private val differentialDriveOdometry = DifferentialDriveOdometry(
         Rotation2d.fromDegrees(imu.angle),
@@ -65,7 +72,17 @@ object DriveSubsystem : Subsystem {
     )
 
     // TODO: Measure track width
-    private val differentialDriveKinematics = DifferentialDriveKinematics(21.8 * METER_CONVERSION_FACTOR)
+    private val differentialDriveKinematics = DifferentialDriveKinematics(TRACK_WIDTH * METER_CONVERSION_FACTOR)
+
+    // See: https://docs.wpilib.org/en/stable/docs/software/advanced-controls/introduction/introduction-to-feedforward.html
+    private var leftFeedforward = SimpleMotorFeedforward(0.0, 0.0, 0.0)
+    private var rightFeedforward = SimpleMotorFeedforward(0.0, 0.0, 0.0)
+
+    private const val DRIVE_P = 0.05
+    private const val DRIVE_I = 0.0
+    private const val DRIVE_D = 0.0
+    internal val leftPIDController = PIDController(DRIVE_P, DRIVE_I, DRIVE_D)
+    internal val rightPIDController = PIDController(DRIVE_P, DRIVE_I, DRIVE_D)
 
     val pose: Pose2d
         get() = differentialDriveOdometry.poseMeters
@@ -88,6 +105,16 @@ object DriveSubsystem : Subsystem {
 
         leftEncoder.reset()
         rightEncoder.reset()
+
+        SmartDashboard.putData("Left PID Controller", leftPIDController)
+        SmartDashboard.putData("Right PID Controller", rightPIDController)
+        SmartDashboard.putData("Left Encoder", leftEncoder)
+        SmartDashboard.putData("Right Encoder", rightEncoder)
+        SmartDashboard.putData("IMU", imu)
+
+        DataTable.staticGainEntry.setDouble(leftFeedforward.ks)
+        DataTable.velocityGainEntry.setDouble(leftFeedforward.kv)
+        DataTable.accelerationGainEntry.setDouble(leftFeedforward.ka)
     }
 
     override fun periodic() {
@@ -96,6 +123,21 @@ object DriveSubsystem : Subsystem {
             leftEncoder.distance * METER_CONVERSION_FACTOR,
             rightEncoder.distance * METER_CONVERSION_FACTOR
         )
+
+        DataTable.leftVoltageEntry.setDouble(leftMotor.motorOutputVoltage)
+        DataTable.rightVoltageEntry.setDouble(rightMotor.motorOutputVoltage)
+        DataTable.leftVelocityEntry.setDouble(leftEncoder.rate)
+        DataTable.rightVelocityEntry.setDouble(rightEncoder.rate)
+
+        val staticGain = DataTable.staticGainEntry.getDouble(leftFeedforward.ks)
+        val velocityGain = DataTable.velocityGainEntry.getDouble(leftFeedforward.kv)
+        val accelerationGain = DataTable.accelerationGainEntry.getDouble(leftFeedforward.ka)
+
+        if (leftFeedforward.ks != staticGain || leftFeedforward.kv != velocityGain || leftFeedforward.ka != accelerationGain)
+            leftFeedforward = SimpleMotorFeedforward(staticGain, velocityGain, accelerationGain)
+
+        if (rightFeedforward.ks != staticGain || rightFeedforward.kv != velocityGain || rightFeedforward.ka != accelerationGain)
+            rightFeedforward = SimpleMotorFeedforward(staticGain, velocityGain, accelerationGain)
     }
 
     fun resetPose(pose: Pose2d) {
@@ -105,22 +147,18 @@ object DriveSubsystem : Subsystem {
     fun driveRelative(relativeSpeeds: ChassisSpeeds) {
         val wheelSpeeds = differentialDriveKinematics.toWheelSpeeds(relativeSpeeds)
 
-        leftPIDController.setpoint = wheelSpeeds.leftMetersPerSecond * INCH_CONVERSION_FACTOR
-        rightPIDController.setpoint = wheelSpeeds.rightMetersPerSecond * INCH_CONVERSION_FACTOR
+        val leftFed = leftFeedforward.calculate(wheelSpeeds.leftMetersPerSecond)
+        val rightFed = rightFeedforward.calculate(wheelSpeeds.rightMetersPerSecond)
 
-        val leftOutput = leftPIDController.calculate(leftEncoder.rate).coerceIn(-1.0, 1.0)
-        val rightOutput = rightPIDController.calculate(rightEncoder.rate).coerceIn(-1.0, 1.0)
+        val leftOutput = leftPIDController.calculate(leftEncoder.rate, wheelSpeeds.leftMetersPerSecond)
+        val rightOutput = rightPIDController.calculate(rightEncoder.rate, wheelSpeeds.rightMetersPerSecond)
 
-        differentialDrive.tankDrive(leftOutput, rightOutput)
+        leftMotor.setVoltage(leftFed + leftOutput)
+        rightMotor.setVoltage(rightFed + rightOutput)
     }
 
     fun arcadeDrive(forwardSpeed: Double, rotationSpeed: Double) {
         var zRotation = rotationSpeed
-
-        if (abs(zRotation) < ROTATION_DEADBAND) {
-            val distanceError = leftEncoder.distance - rightEncoder.distance
-            zRotation = driftCorrectionPID.calculate(distanceError).coerceIn(-1.0, 1.0)
-        }
 
         differentialDrive.arcadeDrive(forwardSpeed, zRotation)
     }
