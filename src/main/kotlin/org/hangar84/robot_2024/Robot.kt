5f144b247a4fafd47ae662248b0bc373076f22db
Cypheriel <61402923+Cypheriel@users.noreply.github.com>
2024-03-22 22:50:22 -0600

    refactor: grab `autonomousCommand` directly from `RobotContainer`

--- src/main/kotlin/org/hangar84/robot_2024/Robot.kt
+++ src/main/kotlin/org/hangar84/robot_2024/Robot.kt
@@ -5,13 +5,9 @@ import edu.wpi.first.hal.FRCNetComm.tResourceType
 import edu.wpi.first.hal.HAL
 import edu.wpi.first.wpilibj.TimedRobot
 import edu.wpi.first.wpilibj.util.WPILibVersion
-import edu.wpi.first.wpilibj2.command.Command
 import edu.wpi.first.wpilibj2.command.CommandScheduler
 
 object Robot : TimedRobot() {
-    private var autonomousCommand: Command? = null
-
-
     override fun robotInit() {
         HAL.report(tResourceType.kResourceType_Language, tInstances.kLanguage_Kotlin, 0, WPILibVersion.Version)
         HAL.report(tResourceType.kResourceType_RobotDrive, tInstances.kRobotDrive_ArcadeStandard)
@@ -34,7 +30,7 @@ object Robot : TimedRobot() {
     }
 
     override fun teleopInit() {
-        autonomousCommand?.cancel()
+        RobotContainer.autonomousCommand.cancel()
     }
 
     override fun teleopPeriodic() {
